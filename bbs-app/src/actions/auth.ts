557e9128e32f3611b45928c9e4ea3a6b97561247
imunida <hirokazu.official@gmail.com>
2026-03-23 00:01:12 +0900

    24. ユーザー登録機能1

--- bbs-app/src/actions/auth.ts
+++ bbs-app/src/actions/auth.ts
@@ -1,37 +1,47 @@
-// 'use server';
+'use server'
 
-// import { redirect } from 'next/navigation';
-// import bcrypt from 'bcryptjs';
-// import { User } from '@/entities/User';
-// import { getRepository } from '@/utils/data-source';
-// import { createSession, deleteSession } from '@/utils/session';
+import { redirect } from 'next/navigation'
+import bcrypt from 'bcryptjs'
+import { User } from '@/entities/User'
+import { getRepository } from '@/utils/data-source'
+import { createSession, deleteSession } from '@/utils/session'
 
-// export async function signup(formData: FormData) {
-//   try {
-//     const userRepository = await getRepository(User);
+export async function signup(formData: FormData) {
+	const userName = formData.get('username') as string
+	const email = formData.get('email') as string
+	const password = formData.get('password') as string
+	if (!userName || !email || !password) {
+		return { error: 'すべてのフィールドを入力してください' }
+	}
 
-//     // メールアドレスの重複チェック
-//     const existingUser = await userRepository.findOneBy({ email });
-//     if (existingUser) {
-//       return { error: 'このメールアドレスは既に使用されています' };
-//     }
+	try {
+		const userRepository = await getRepository(User)
 
-//     // パスワードのハッシュ化
-//     const hashedPassword = await bcrypt.hash(password, 10);
+		// メールアドレスの重複チェック
+		const existingUser = await userRepository.findOneBy({ email })
+		if (existingUser) {
+			return { error: 'このメールアドレスは既に使用されています' }
+		}
 
-//     // ユーザー作成
-//     const newUser = userRepository.create({
-//       userName,
-//       email,
-//       password: hashedPassword,
-//     });
+		// パスワードのハッシュ化
+		const hashedPassword = await bcrypt.hash(password, 10)
 
-//     await userRepository.save(newUser);
-//   } catch (e) {
-//     console.error(e);
-//     return { error: 'ユーザー登録中にエラーが発生しました' };
-//   }
-// }
+		// ユーザー作成
+		const newUser = userRepository.create({
+			userName,
+			email,
+			password: hashedPassword,
+		})
+		// saveを実行するとDBに反映される
+		await userRepository.save(newUser)
+
+		// ここでトップページに戻る処理redirect
+		redirect('/')
+	} catch (e) {
+		console.error(e)
+		return { error: 'ユーザー登録中にエラーが発生しました' }
+	}
+}
 
 // export async function login(formData: FormData) {
 //   try {
